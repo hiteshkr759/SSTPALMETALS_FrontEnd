e65df14b08bb7711577ee63a4c48980a064a0bdb
Hitesh Kumar <hiteshkr759@gmail.com>
2020-04-26 13:32:30 +0530

    Init App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path:'inventory',
+       // component:HomeComponent
+        //loadChildren : './home/home.module#HomeModule'
+        loadChildren : () => import('./inventory/inventory.module').then(m=>m.InventoryModule)
+      },
+      // {
+      //   path:'dashboard',
+      //  // component:HomeComponent
+      //   //loadChildren : './dashboard/dashboard.module#DashboardModule',
+      //   loadChildren :  () => import('./dashboard/dashboard.module').then(m=>m.DashboardModule)
+      // },
+      {
+        path:'**',
+        redirectTo:'/inventory/billing',
+        pathMatch:'full'
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
